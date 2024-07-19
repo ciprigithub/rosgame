9b941292e6dbc1e80ca4ed3030c4a3b344b9fd69
Cipriano Galindo <cga@uma.es>
2024-07-19 12:53:44 +0200

    Elusive players are added

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # rosgame
+This package implements a game manager in which players are robots that must survive in a tournament arena. 
+
+The game is played in a simulated world using CoppeliaSim. Players, ROS2 nodes, get world information like laser data, odometry and information about near items. Available items are battery recharge, shield, hammer, and automatic navigation.
+
+To test the package, three different types of players are implemented:
+
+- Pacific players that will navigate searching for battery recharge points and are not interested in having items
+- War players that will search hammers and oponents
+- Elusive players that are only interested in avoiding obstacles.
+
+How to test it?
+
 1.- Launch tfg_scene in Coppelia (TODO change the name :))
 2.- Run bridge node from rosgame_bridge package
 3.- Launch one of the launch files from the rosgame_players package
---
+++ rosgame_players/src/elusive_player.cpp
@@ -0,0 +1,136 @@
+#include "player_definition.hpp"
+
+Player::Player(): Node ("elusive_player")
+{
+    // ...
+}
+
+
+void Player::process_laser_info(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+{
+    // Se busca la distancia mínima, que representa el objeto más cercano.
+    std::vector<float>::iterator min_it = std::min_element(msg->ranges.begin()+25, msg->ranges.end()-25);
+
+    double nearest_obstacle_distance = *min_it;
+    int pos_in_array = std::distance(msg->ranges.begin(), min_it);
+
+    float linear,angular;
+    angular=0;
+    linear=1;
+    if(pos_in_array<=(msg->ranges.size()/2)-25)
+    {
+        //Obstáculo ubicado en la zona derecha
+        angular=0.5;
+    }
+    if(pos_in_array>=(msg->ranges.size()/2)+25)
+    {
+        //Obstáculo ubicado en la zona izq
+        angular=-0.5;
+    }
+    if (nearest_obstacle_distance<0.5)
+    {
+        linear=-0.5;
+    }
+    publish_vel(linear,angular);
+}
+
+
+void Player::process_scene_info(const std_msgs::msg::String::SharedPtr msg)
+{
+    // Se convierte el msg de tipo string con formato en un JSON.
+    Json::CharReaderBuilder reader;
+    Json::Value JsonSceneData;
+    std::istringstream jsonStream(msg->data);
+    Json::parseFromStream(reader, jsonStream, &JsonSceneData, nullptr);
+        
+    // Se obtiene el valor de la batería de la clave "Battery_Level".
+    battery = JsonSceneData["Battery_Level"].asFloat();
+
+    // Se obtiene la pose del robot a partir de la clave "Robot_Pose".
+    pos_x = JsonSceneData["Robot_Pose"]["x"].asFloat();
+    pos_y = JsonSceneData["Robot_Pose"]["y"].asFloat();
+    gamma = JsonSceneData["Robot_Pose"]["gamma"].asFloat();
+
+    // Se obtienen las habilidades de la cave "Skills".
+    autopilot_enabled = JsonSceneData["Skills"]["Autopilot"].asBool();
+    hammer_enabled = JsonSceneData["Skills"]["Hammer"].asBool();
+    shield_enabled = JsonSceneData["Skills"]["Shield"].asBool();
+
+    // Se obtienen las posiciones de bloques de habilidades de la clave "Skills_Positions" en el campo "FOV".
+    std::vector<std::vector<float>> skills_pos_array_aux;
+    const Json::Value &skills_pos = JsonSceneData["FOV"]["Skills_Positions"];
+    for (const Json::Value &skill : skills_pos)
+    {
+        std::vector<float> skillData;
+        for (const Json::Value &value : skill)
+        {   skillData.push_back(value.asFloat());   }
+        skills_pos_array_aux.push_back(skillData);
+    }
+    skills_pos_array = skills_pos_array_aux;
+
+    // Se obtienen las posiciones de plataformas de recarga de la clave "Chargers_Positions" en el campo "FOV".
+    // Las plataformas de recarga están en posiciones fijas durante toda la simulación.
+    // Objetivo: buscar las cinco plataformas y almacenarlas todas en la lista "chargers_pos_array".
+    const Json::Value &chargers_pos = JsonSceneData["FOV"]["Chargers_Positions"];
+    for (const Json::Value &charger : chargers_pos)
+    {
+        std::vector<float> chargerData;
+        for (const Json::Value &value : charger)
+        {   chargerData.push_back(value.asFloat());     }
+
+        // Verifica si la posición ya está en "chargers_pos_array".
+        // La función "find" devuelve un iterador que apunta al elemento del array si existe o al final del array si no lo ha encontrado.
+        if (std::find(chargers_pos_array.begin(), chargers_pos_array.end(), chargerData) == chargers_pos_array.end())
+        {   chargers_pos_array.push_back(chargerData);  }
+    }
+
+    // Se obtienen las posiciones de los oponentes de la clave "Players_Positions" en el campo "FOV".
+    std::vector<std::vector<float>> players_pos_array_aux;
+    const Json::Value &players_pos = JsonSceneData["FOV"]["Players_Positions"];
+    for (const Json::Value &player : players_pos)
+    {
+        std::vector<float> playerData;
+        for (const Json::Value &value : player)
+        {   playerData.push_back(value.asFloat());  }
+        players_pos_array_aux.push_back(playerData);
+    }
+    players_pos_array = players_pos_array_aux;
+    
+    // DEBUGGING
+    /*
+    RCLCPP_INFO(this->get_logger(), "Msg: '%s'", msg->data.c_str());
+
+    RCLCPP_INFO(this->get_logger(), "Battery level: '%f'", battery);
+
+    RCLCPP_INFO(this->get_logger(), "Is teleport enabled? '%s'", (teleport_enabled ? "True" : "False"));
+    RCLCPP_INFO(this->get_logger(), "Is the hammer enabled? '%s'", (hammer_enabled ? "True" : "False"));
+    RCLCPP_INFO(this->get_logger(), "Is the shield enabled? '%s'", (shield_enabled ? "True" : "False"));
+
+    if (!skills_pos_array.empty())
+    {
+        RCLCPP_INFO(this->get_logger(), "Skills Positions Array:");
+        for (const auto &skill_pos : skills_pos_array)
+        {   RCLCPP_INFO(this->get_logger(), "[X] = '%f', [Y] = '%f'", skill_pos[0], skill_pos[1]);  }
+    }
+
+    RCLCPP_INFO(this->get_logger(), "Robot Pose: [X] = '%f', [Y] = '%f', [GAMMA] = '%f'", pos_x, pos_y, gamma);
+    */
+}
+
+
+std::tuple<double, double, double, double> Player::autonomous_navigation(double nearest_obstacle_distance)
+{
+    
+}
+
+
+void Player::publish_vel(double linear, double angular)
+{
+    rosgame_msgs::msg::RosgameTwist cmd_vel;
+
+    cmd_vel.vel.linear.x = linear;
+    cmd_vel.vel.angular.z = angular;
+    cmd_vel.code = code;
+
+    pub1_->publish(cmd_vel);
+}
