455c3033d5f38835d2fbd73fabb4abf23b4cce13
jgmonroy <jgmonroy@uma.es>
2024-10-25 11:10:08 +0200

    New pkg warrior-monroy (for testing)

---
+++ warrior_monroy/src/main_warrior.cpp
@@ -0,0 +1,25 @@
+//--------------------------------------------------------------------//
+// University of Málaga
+// MAPIR Research Group - Machine Perception and Intelligent Robotics
+//--------------------------------------------------------------------//
+
+#include "warrior_monroy/warrior.hpp"
+
+int main ( int argc, char * argv[] )
+{
+    rclcpp::init ( argc, argv );
+    
+    auto node=std::make_shared<Warrior>();
+
+    rclcpp::Rate rate(1);
+
+    while(rclcpp::ok())
+    {
+        rclcpp::spin_some(node);
+        node->publish_twist();
+        rate.sleep();
+    }
+    
+    rclcpp::shutdown();
+    return 0;
+}
